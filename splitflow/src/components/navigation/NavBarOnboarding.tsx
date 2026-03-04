42d40992bd1b2a718a1754ecc79153a1768b6788
Erwin K <e.kuechel@gmail.com>
2026-03-04 22:27:13 +0100

    style: update names in NavBarOnboarding

--- splitflow/src/components/navigation/NavBarOnboarding.tsx
+++ splitflow/src/components/navigation/NavBarOnboarding.tsx
@@ -2,16 +2,16 @@ import Button from "../ui/Button";
 import Logo from "../ui/Logo";
 
 interface Props {
-	handleClick: () => void;
+	onClick: () => void;
 }
 
-const NavBarOnboarding = ({ handleClick }: Props) => {
+const NavBarOnboarding = ({ onClick }: Props) => {
     return (
 			<div className='flex justify-between'>
 				<Logo />
 				<div className="flex items-center gap-4">
-					<Button variant="onlyText" className="hidden sm:block" onClick={handleClick}>Skip Intro</Button>
-					<Button variant='secondary'>Login</Button></div>
+					<Button variant="transparent" className="hidden sm:block" onClick={onClick}>Skip Intro</Button>
+					<Button variant='secondary' onClick={onClick}>Login</Button></div>
 			</div>
     );
 };
