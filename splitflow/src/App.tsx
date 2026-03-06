0350de512e872c93ff0da11a09b2501df172df8b
Erwin K <e.kuechel@gmail.com>
2026-03-06 17:58:09 +0100

    chore: add new Pages into Route @ entrypoint

--- splitflow/src/App.tsx
+++ splitflow/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import OnboardingPage from './pages/OnboardingPage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import DashboardPage from './pages/DashboardPage';
+import AuditPage from './pages/AuditPage';
 
 function App() {
 	return (
@@ -10,6 +12,8 @@ function App() {
 				<Route path="/" element={<OnboardingPage />} />
 				<Route path="/login" element={<LoginPage />} />
 				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/dashboard" element={<DashboardPage />} />
+				<Route path="/audit" element={<AuditPage />} />
 			</Routes>
 	)
 }
