f3db5949e7db8250afa3bdf34c29f836b4ec2103
Erwin K <e.kuechel@gmail.com>
2026-03-09 22:01:15 +0100

    feat: add a mainContainer
    it allows the sidebar to display correctly on XL breakpoint

---
+++ splitflow/src/components/ui/MainContainer.tsx
@@ -0,0 +1,22 @@
+
+interface Props extends React.PropsWithChildren {
+	columnsNum: number;
+}
+
+const colMap: Record<number, string> = {
+  1: "xl:grid-cols-1",
+  2: "xl:grid-cols-2",
+  3: "xl:grid-cols-3",
+  4: "xl:grid-cols-4",
+};
+
+const MainContainer = ({ columnsNum=2, children }: Props) => {
+  return (
+		<div className={`xl:pl-64 sm:px-8 pb-12 grid grid-cols-1 ${colMap[columnsNum]} gap-8 items-start`}>
+    	{ children } 
+    </div>
+  );
+};
+
+export default MainContainer;
+
